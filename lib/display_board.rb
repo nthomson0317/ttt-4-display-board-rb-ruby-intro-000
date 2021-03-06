64087367e2f778829cf030bc7ca18a22f0d7a576
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-03-06 17:59:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
+
+  board =[" "," "," "," "," "," "," "," "," "]
+  
   puts "board#{[0]}" | "board#{[1]}" | "board#{[2]}"
   puts "-----------"
   puts "board#{[3]}" | "board#{[4]}" | "board#{[5]}"
@@ -8,7 +11,7 @@ def display_board(board)
   puts "board#{[6]}" | "board#{[7]}" | "board#{[8]}"
 
 
-board =[" "," "," "," "," "," "," "," "," "]
+
 
 end
 
