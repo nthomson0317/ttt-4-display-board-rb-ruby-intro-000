a2772d545c5df6ecf752054be8cf8815a9938f23
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-03-06 18:23:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,14 +2,12 @@
 # out the current state.
 def display_board(board)
 
-  board =[" "," "," "," "," "," "," "," "," "]
 
-
-  puts "board#{[0]}" | "board#{[1]}" | "board#{[2]}"
+  puts "#{board[0]}" | "#{board[1]}" | "#{board[2]}"
   puts "-----------"
-  puts "board#{[3]}" | "board#{[4]}" | "board#{[5]}"
+  puts "#{board[3]}" | "#{board[4]}" | "#{board[5]}"
   puts "-----------"
-  puts "board#{[6]}" | "board#{[7]}" | "board#{[8]}"
+  puts "#{board[6]}" | "#{board[7]}" | "#{board[8]}"
 
   puts "X#{[0]}"
   puts "O#{[0]}"
@@ -20,6 +18,9 @@ end
 
 puts "Turn 1 Where Would You Like to Go?"
 
-  board = [" "," "," "," "," "," "," "," "," "]
 
+  board = [" "," "," "," "," "," "," "," "," "]
+  puts board[0]
 display_board(board)
+
+
