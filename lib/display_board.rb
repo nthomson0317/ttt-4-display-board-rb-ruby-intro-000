3d5fe539543ce096d1a242b9ba9365ca0291cdc9
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-03-06 18:01:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,13 +4,15 @@ def display_board(board)
 
   board =[" "," "," "," "," "," "," "," "," "]
 
+
   puts "board#{[0]}" | "board#{[1]}" | "board#{[2]}"
   puts "-----------"
   puts "board#{[3]}" | "board#{[4]}" | "board#{[5]}"
   puts "-----------"
   puts "board#{[6]}" | "board#{[7]}" | "board#{[8]}"
 
-
+  puts "X#{[0]}"
+  puts "O#{[0]}"
 
 
 end
@@ -22,5 +24,4 @@ puts "Turn 1 Where Would You Like to Go?"
 
 display_board(board)
 
-puts "X#{[0]}"
-puts "O#{[0]}"
+
